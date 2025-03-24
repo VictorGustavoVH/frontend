6e9da24ccdb68d32fcf7bc3dc35ce7142cb4ecff
VictorGustavoVH <20230057@uthh.edu.mx>
2025-03-24 06:05:01 -0600

    ssss

--- src/views/private/ProductCrud.tsx
+++ src/views/private/ProductCrud.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   getProducts,
-  registerProduct,
+ 
   uploadProductImage,
   updateProduct as apiUpdateProduct,
   deleteProduct as apiDeleteProduct
